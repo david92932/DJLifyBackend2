48b567dcde5826d8ef8a5d85aa9424363f8f4f89
JacksonB21 <39737224+JacksonB21@users.noreply.github.com>
2019-12-06 13:29:26 -0500

    SongModel + DJModel finished
    song and DJ class updated

--- src/main/java/com/djlify/demo/Models/DJModel.java
+++ src/main/java/com/djlify/demo/Models/DJModel.java
@@ -4,13 +4,75 @@ import java.util.ArrayList;
 
 public class DJModel {
 
-    public String name;
-    public String biography;
+    public String fname, lname, ID, pictureURL, biography;
 
     public ArrayList<EventModel> events = new ArrayList<EventModel>();
+
+
+
+    DJModel(){
+        this.fname = null;
+        this.lname = null;
+        this.ID = null;
+        this.pictureURL = null;
+        this.biography = null;
+
+    }
+
+    DJModel(String fname, String lname, String ID, String pictureURL, String biography)
+    {
+        this.fname = fname;
+        this.lname = lname;
+        this.ID = ID;
+        this.pictureURL = pictureURL;
+        this.biography = biography;
+    }
 
     public void addEvent(EventModel event) {
         this.events.add(event);
     }
 
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public void setPictureURL(String pictureURL) {
+        this.pictureURL = pictureURL;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getPictureURL() {
+        return pictureURL;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public ArrayList<EventModel> getEvents() {
+        return events;
+    }
 }
--- src/main/java/com/djlify/demo/Models/SongModel.java
+++ src/main/java/com/djlify/demo/Models/SongModel.java
@@ -1,4 +1,67 @@
 package com.djlify.demo.Models;
+import java.lang.String;
+import java.util.Date;
 
 public class SongModel {
+
+    String name, ID, APIURL, artist;
+    Date duration;
+
+    SongModel()
+    {
+        this.name = null;
+        this.artist = null;
+        this.ID = null;
+        this.APIURL = null;
+        this.duration = null;
+    }
+
+    SongModel(String name, String artist, String ID, String APIURL, Date duration)
+    {
+        this.name = name;
+        this.artist = artist;
+        this.ID = ID;
+        this.APIURL = APIURL;
+        this.duration = duration;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setAPIURL(String APIURL) {
+        this.APIURL = APIURL;
+    }
+
+    public void setDuration(Date duration) {
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getAPIURL() {
+        return APIURL;
+    }
+
+    public Date getDuration() {
+        return duration;
+    }
 }
