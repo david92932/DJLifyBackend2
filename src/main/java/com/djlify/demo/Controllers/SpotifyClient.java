55db0d0c708b29a41e348d88ded13b035cc52e5a
JacksonB21 <39737224+JacksonB21@users.noreply.github.com>
2019-12-10 20:39:53 -0500

    Search for song created
    searches for song and returns list of Track[] objects.

--- src/main/java/com/djlify/demo/Controllers/SpotifyClient.java
+++ src/main/java/com/djlify/demo/Controllers/SpotifyClient.java
@@ -1,6 +1,41 @@
 package com.djlify.demo.Controllers;
 
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.Track;
+import com.wrapper.spotify.requests.data.search.simplified.SearchTracksRequest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class SpotifyClient {
 
-    
+    private static final String clientID = "172db9e2b2df4acda5cc2fb603038ddd";
+
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(clientID).build();
+
+    private static  SearchTracksRequest searchTrackRequest;
+
+    public static ArrayList<Track[]> searchSong(String searchAttribute)
+    {
+
+        searchTrackRequest = spotifyApi.searchTracks(searchAttribute).build();
+        ArrayList<Track[]> list = new ArrayList<>();
+
+        try {
+
+             Paging<Track> trackPaging = searchTrackRequest.execute();
+             Track[] items = trackPaging.getItems();
+             list.add(items);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SpotifyWebApiException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
 }
